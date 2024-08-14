6993a9efe9b02997671b33b4fd153f4030ec78d8
yslamartins <claudiaysla101@gmail.com>
2024-08-14 10:20:32 -0300

    Vinculo com o swagger para gerar a documentação da api durante o fluxo de trabalho

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Role } from "../entities/user.entity";
 
 export class CreateUserDto {
+    @ApiProperty({example: 'John'})
     readonly name: string;
+
+    @ApiProperty({example: 'john@example.com'})
     readonly email: string;
+
+    @ApiProperty({example: 'pass123'})
     readonly password: string;
-    role: Role[];
+
+    @ApiProperty({
+        example: 0,
+        description: 'Role of the user (0: Admin, 1: Manager, 2: User)',
+      })
+      role: number;
 }
